e4e8b479b9ddbdf28b2d667764e80ecdea70b1ae
이름 <>
2026-03-04 23:12:05 +0900

    레벨 1. ArgumentResolver

---
+++ src/main/java/org/example/expert/config/WebConfig.java
@@ -0,0 +1,21 @@
+package org.example.expert.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthUserArgumentResolver authUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authUserArgumentResolver); // 여기서 등록해줘야 합니다!
+    }
+}
